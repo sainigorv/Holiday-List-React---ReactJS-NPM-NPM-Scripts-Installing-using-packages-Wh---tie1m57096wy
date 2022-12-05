6b5dc2f593521b603fa954b67c909b968fd949f5
sainigorv <sainigorv341@gmail.com>
2022-12-05 22:02:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 // function generateCodeFromObject(obj){
-//return a code generated string
+//     //return a code generated string
 //    }
 
 // module.exports=generateCodeFromObject;
